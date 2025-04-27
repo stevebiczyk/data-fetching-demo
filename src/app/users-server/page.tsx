dc8b2fe164881347834c983f792e9f62af8ae9f3
stevebiczyk <stevebiczyk@gmail.com>
2025-04-27 23:44:52 +0100

    Loading and Error States

--- src/app/users-server/page.tsx
+++ src/app/users-server/page.tsx
@@ -7,6 +7,7 @@ type User = {
 };
 
 export default async function UsersServer() {
+  await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate a delay
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await response.json();
 
