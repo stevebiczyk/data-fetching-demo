24eda5c725fe6e9450f0785d42b47f1245f0acf4
stevebiczyk <stevebiczyk@gmail.com>
2025-05-15 17:28:22 +0100

    Form Validation with useActionState

--- src/app/products-db-create/page.tsx
+++ src/app/products-db-create/page.tsx
@@ -1,8 +1,28 @@
 import { Submit } from "@/components/submit";
 import { addProduct } from "@/prisma-db";
 import { redirect } from "next/navigation";
+import { useActionState } from "react";
+
+type Errors = {
+  title?: string;
+  price?: string;
+  description?: string;
+};
+
+type FormState = {
+  errors: Errors;
+};
 
 export default function AddProductPage() {
+  const initialState: FormState = {
+    errors: {},
+  };
+
+  const [state, formAction, isPending] = useActionState(
+    createProduct,
+    initialState
+  );
+
   async function createProduct(formData: FormData) {
     "use server";
 
@@ -10,36 +30,37 @@ export default function AddProductPage() {
     const price = formData.get("price") as string;
     const description = formData.get("description") as string;
 
+    const errors: Errors = {};
+    if (!title) {
+      errors.title = "Title is required";
+    }
+
+    if (!price) {
+      errors.price = "Price is required";
+    }
+
+    if (!description) {
+      errors.description = "Description is required";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return { errors };
+    }
+
     await addProduct(title, parseInt(price), description);
     redirect("/products-db");
-
-    // // Basic validation
-    // const errors: Record<string, string> = {};
-    // if (!title) errors.title = "Title is required";
-    // if (!price || isNaN(Number(price)))
-    //   errors.price = "Valid price is required";
-    // if (!description) errors.description = "Description is required";
-
-    // if (Object.keys(errors).length > 0) {
-    //   return { errors };
-    // }
-
-    // // Simulate a database save operation
-    // console.log("Product created:", { title, price, description });
-
-    // return { success: true };
   }
 
-  const state = {
-    errors: {
-      title: "",
-      price: "",
-      description: "",
-    },
-  };
+  //   const state = {
+  //     errors: {
+  //       title: "",
+  //       price: "",
+  //       description: "",
+  //     },
+  //   };
 
   return (
-    <form action={createProduct} className="p-4 space-y-4 max-w-96">
+    <form action={formAction} className="p-4 space-y-4 max-w-96">
       <div>
         <label className="text-black">
           Title
